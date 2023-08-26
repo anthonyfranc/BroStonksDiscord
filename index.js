917892c80b647f9969b52d7d87e609b227481971
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-08-25 20:48:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const wss = new WebSocket.Server({ server });
 
 const { createClient } = require("@supabase/supabase-js");
 
-const supabaseUrl = "https://jjtqvxvprcmblezstaks.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpqdHF2eHZwcmNtYmxlenN0YWtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE3NjAxMjAsImV4cCI6MjAwNzMzNjEyMH0.glxbp12RNVsu6TaSqPGH_CUDs9AH7T1jNkfwLtz3ZQI";
+const supabaseUrl = "http://157.230.89.176:8000";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNjkyOTM2MDAwLAogICJleHAiOiAxODUwNzg4ODAwCn0.xnHurrvhEvLxHKWRiHEeD-r5SVdOU-NqMgz5mB8BqG8";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const sdk = require("api")("@mobula-api/v1.0#4cpc4om4lkxxs6mc");
