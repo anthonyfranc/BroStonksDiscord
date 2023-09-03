4883985847d1dc67881b7a13434d38c56f9b49bb
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-09-03 19:54:50 +0000

    Updates

---
+++ discord.mjs
@@ -0,0 +1,70 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import { WebSocket } from 'ws';
+import http from 'http';
+
+const server = http.createServer();
+const wss = new WebSocket.Server({ server });
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+
+// Keep track of connected WebSocket clients
+const connectedClients = new Set();
+
+wss.on('connection', (ws) => {
+  console.log('Connected to WebSocket server');
+
+  // Add the connected client to the set
+  connectedClients.add(ws);
+
+  ws.on('close', () => {
+    console.log('WebSocket client disconnected');
+    // Remove the disconnected client from the set
+    connectedClients.delete(ws);
+  });
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.channel.id === '1146980018631737386') {
+    console.log(`${message.author.tag}: ${message.content}`);
+    
+    // Check if there are connected WebSocket clients
+    if (connectedClients.size > 0) {
+      const messageData = {
+        author: message.author.tag,
+        content: message.content,
+        timestamp: message.createdAt.toISOString(), // Include the timestamp
+      };
+      
+      // Send the message data to all connected clients
+      connectedClients.forEach((ws) => {
+        ws.send(JSON.stringify(messageData));
+      });
+    }
+  }
+});
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login('MTE0NzEzNzQxNTk4MzY2MTIzNw.GuWxaL.bxExuFOBeKJe91c34mra63sBj93GN1WrVUPqX0'); // Replace with your bot token
+
+const PORT = process.env.PORT || 4100;
+server.listen(PORT, () => {
+  console.log(`Node server listening on port ${PORT}`);
+});
